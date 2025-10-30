5a7ab054e525508e40fc23065888b7e14252a665
Revmagi <Revmagi@users.noreply.github.com>
2025-10-30 11:13:17 -0400

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,8 @@
+"""
+DDM_Bundle - Custom ComfyUI Nodes
+Set and Get nodes for clean workflow routing
+"""
+
+from .ddm_nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
